066e8a3c1b0db04fc105543c30f23be7b866a855
arjunbector <arjunbector@gmail.com>
2023-10-26 00:50:48 +0530

    Added api call

--- src/Components/Meme/Meme.jsx
+++ src/Components/Meme/Meme.jsx
@@ -1,38 +1,76 @@
-import React, { useState } from 'react'
-import styles from './Meme.module.css';
-import memeData from '../../Data/memeData.js'
+import React, { useState, useEffect } from "react";
+import styles from "./Meme.module.css";
 
 const Meme = () => {
-  const [memeImage, setMemeImage] = useState("")
-  const getMemeImage = ()=>{
-    const memesArray = memeData.data.memes;
-    const randomNumber = Math.floor(Math.random()*memesArray.length)
-    setMemeImage(memesArray[randomNumber].url);
-    
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+  const [allMemeImages, setAllMemeImages] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res=>res.json())
+    .then(data=>{
+      setAllMemeImages(data);
+      console.log(data);
+    });
+  },[])
+  
+  const handleChange = (event)=>{
+    const {name, value} = event.target;
+    setMeme((prev)=>({
+      ...prev,
+      [name] : value,
+
+    }))
   }
+
+  const getMemeImage = () => {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prev) => ({
+      ...prev,
+      randomImage: url,
+    }));
+  };
+
   return (
     <div className={styles.formDiv}>
       <div className={styles.form}>
         <div className={styles.inputsBox}>
-            <input
-            type="text" 
-            placeholder='Top text'
-            />
-            <input
-            type="text"
-            placeholder='Bottom text' />
+          <input
+          type="text" 
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+          />
+          <input 
+          type="text" 
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+          />
         </div>
-        <button 
-        className={styles.btn}
-        onClick={getMemeImage}>
-            Get new meme image
+        <button className={styles.btn} onClick={getMemeImage}>
+          Get new meme image
         </button>
-        <div className={styles.memeImage}>
-          <img src={memeImage} alt="" />
+        <div className={styles.meme}>
+          {meme.randomImage && (
+            <div className={styles.memeImage}>
+              <img src={meme.randomImage} alt="" />
+              <h2 className={styles.meme_top_text}>{meme.topText}</h2>
+              <h2 className={styles.meme_bottom_text}>{meme.bottomText}</h2>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};  
 
-export default Meme
+export default Meme;
